56f2a5b7875ccc88f88930f36bf5b001eb36825c
Jay Ravani <jaykumar.ravani@th-deg.de>
2026-08-05 19:45:07 +0200

    feat: add versioning support with version, commit, and date information

--- cmd/ignis/main.go
+++ cmd/ignis/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/thd-spatial-ai/ignis/internal/api/server"
 	"github.com/thd-spatial-ai/ignis/internal/config"
 	"github.com/thd-spatial-ai/ignis/internal/utils"
+	"github.com/thd-spatial-ai/ignis/internal/version"
 )
 
 // Setup app server and routes
 func main() {
+	showVersion := flag.Bool("version", false, "print version and exit")
+	flag.BoolVar(showVersion, "v", false, "print version and exit (shorthand)")
+	flag.Parse()
+	if *showVersion {
+		fmt.Printf("%s (commit %s, built %s)\n", version.Version, version.Commit, version.Date)
+		os.Exit(0)
+	}
+
 	utils.InitLogger()
 	cfg := config.LoadConfig()
 	app, cleanup := server.SetupServer()
